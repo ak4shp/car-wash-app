31df06444902ee6a06f926d95cc8a37f2e6201d7
Aakash Patel <98658395+ak4shp@users.noreply.github.com>
2022-12-24 17:55:00 +0530

    Add files via upload

---
+++ setenv.bat
@@ -0,0 +1,3 @@
+set CWA_ENV=dev
+set CWA_DEBUG=True
+set CWA_SECRET="gr7ac^e*dx8c5jq8982e7oga*nn9rkb6n$@-xd-tz_&4xfzma"
---
+++ setenv.sh
@@ -0,0 +1,3 @@
+export CWA_ENV=dev
+export CWA_DEBUG=True
+export CWA_SECRET="gr7ac^e*dx8c5jq8982e7oga*nn9rkb6n$@-xd-tz_&4xfzma"
